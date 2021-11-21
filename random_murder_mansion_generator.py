02403e37e7297cb7eef983b432c585a4fa688a5e
sarahperez <sarahperez@mims.ai>
2021-11-21 12:28:15 -0500

    Reorganize code and add arg parser for random seed and number of sentences

--- random_murder_mansion_generator.py
+++ random_murder_mansion_generator.py
@@ -1,3 +1,7 @@
+import random as rand
+# rand.choice() # with replacement
+# rand.sample() #without replacement
+import argparse
 
 f = open('characters.txt','r')
 characters = [x.strip('\n') for x in f.readlines()]
@@ -14,16 +18,8 @@ items = [x.strip('\n') for x in f.readlines()]
 f = open('sentences.txt','r')
 sentences = [x.strip('\n') for x in f.readlines()]
 
-import random as rand
-
-# rand.choice() # with replacement
-# rand.sample() #without replacement
-
 def capitalizer(s):
 	return s[0].capitalize() + s[1:]
-
-novel = ''
-nb_sentences = 10
 
 objects = {
 	'CHARACTER': characters,
@@ -32,28 +28,44 @@ objects = {
 	'ITEM': items
 }
 
-for _ in range(nb_sentences):
 
-	used = set() # we keep track of characters/items/verbs used so we don't reuse them in the sentence
-	# Pick a sentence
-	sentence = rand.choice(sentences)
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description="Write a murder mystery with certain chacracteristics.")
+	parser.add_argument("--seed", type=int, help="The random seed.", default=None)
+	parser.add_argument("-n", type=int, help="The number of sentences.", default=10)
+	args = parser.parse_args()
+	print("here")
 
-	# for each thing to replace
-	for object_type, object_choices in objects.items():
-		while object_type in sentence:
-			choice = None
-			while choice in used or choice is None:
-				choice = rand.choice(object_choices)
-			sentence = sentence.replace(object_type, choice, 1)
-			used.add(choice)
+	nb_sentences = args.n
+	if args.seed:
+		rand.seed(args.seed)
 
-	# Add to novel
-	novel += '\n' + capitalizer(sentence)
+	novel = "\n"
+	for _ in range(nb_sentences):
 
-print(novel)
+		used = set() # we keep track of characters/items/verbs used so we don't reuse them in the sentence
+		# Pick a sentence
+		sentence = rand.choice(sentences)
+
+		# for each thing to replace
+		for object_type, object_choices in objects.items():
+			while object_type in sentence:
+				choice = None
+				while choice in used or choice is None:
+					choice = rand.choice(object_choices)
+				sentence = sentence.replace(object_type, choice, 1)
+				used.add(choice)
+
+		# Add to novel
+		novel += '\n' + capitalizer(sentence)
+
+	print(novel)
+	
 
 # IDEAS
 
+# Perhaps we have an option to create the novel with a randomly set cast of character/rooms/items
+# so that the repetition creates interesting connections in the reader's mind
 # Each room can have features
 # A color,
 # A temperature/moisture
